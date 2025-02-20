76e610a3595b950c3d88226f55d30ed84b85fd0a
Stepan <fenrird@inbox.ru>
2025-02-20 12:46:57 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,94 @@
+import com.android.build.api.dsl.Packaging
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serializaion)
+}
+
+android {
+    namespace = "com.example.videoapp"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.example.videoapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug{
+            buildConfigField("String","BASE_URL","\"https://furniture-api.fly.dev/v1/\"")
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String","BASE_URL","\"https://furniture-api.fly.dev/v1/\"")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+        }
+    }
+
+}
+
+dependencies {
+    //ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    //coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+    //koin
+    implementation(libs.koin.androidx.compose)
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    //exo player
+    implementation(libs.androidx.media3.exoplayer) // androidx.media3 ExoPlayer dependency
+    implementation(libs.androidx.media3.session) // MediaSession Extension dependency
+    implementation(libs.androidx.media3.ui) //Base Player UI
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/example/videoapp/video/presentation/videoList/VideListScreen.kt
@@ -0,0 +1,11 @@
+package com.example.videoapp.video.presentation.videoList
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun VideListScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
